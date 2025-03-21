ed083dfdeebd7106d5633615c30d9da90bd04395
tiredpancake <119611305+tiredpancake@users.noreply.github.com>
2025-03-21 05:04:47 +0330

    fix : fix darkmode on first two pages

--- src/utils/App.tsx
+++ src/utils/App.tsx
@@ -3,18 +3,21 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../components/Homepage/homepage';
+import {ThemeProvider} from '../components/theme';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
 createRoot(rootElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<HomePage />} />
-          {/* <Route path="blogs" element={<Blogs />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+  <ThemeProvider>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />} />
+            {/* <Route path="blogs" element={<Blogs />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>,
+  </ThemeProvider>
 );  
