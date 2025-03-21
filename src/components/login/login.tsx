0aefbc2cdfa2648ba36edb984da4e7a33a4c20ab
tiredpancake <119611305+tiredpancake@users.noreply.github.com>
2025-03-21 14:43:51 +0330

    fix : fix buttons

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -10,6 +10,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CloseIcon from '@mui/icons-material/Close';
 import PhoneOutlined from '@mui/icons-material/PhoneOutlined';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
 
 import createCache from '@emotion/cache';
 
@@ -34,6 +36,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
    
   const { isDarkMode, toggleDarkMode } = useTheme(); // Get both isDarkMode and toggleDarkMode from context  
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [Name, setName] = useState('');
   const [isFocused, setIsFocused] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [step, setStep] = useState<Step>(Step.PHONE);
@@ -417,7 +420,77 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
     case Step.REGISTER:
       return (
         <>
-        <h1> page three</h1>
+       <div className={`phone-input ${isDarkMode ? 'dark-login' : 'light-login'}`} >
+          <div dir="rtl" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                  
+            <div className="form-group">  
+            <span className="icon"><DriveFileRenameOutlineIcon style={{color: isDarkMode ? '#FFFFFF' : '#4C4343', }} /></span> {/* Phone icon */}  
+            <input  
+                type="text"  
+                id="Name"  
+                placeholder=" "  
+                autoComplete="off"  
+                required  
+                value={Name}  
+                onChange={handleInputChange}  
+                className={error ? 'error' : ''} 
+              />  
+            <label htmlFor="Name" className={error ? 'error' : ''}>نام کاربری</label>  
+            </div> 
+          </div>
+        </div>
+        <div className={`phone-input ${isDarkMode ? 'dark-login' : 'light-login'}`} >
+          <div dir="rtl" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>      
+            <div className="form-group">  
+            <span className="icon"><MarkEmailReadIcon style={{color: isDarkMode ? '#FFFFFF' : '#4C4343', }} /></span> {/* Phone icon */}  
+            <input  
+                type="tel"  
+                id="phoneNumber"  
+                placeholder=" "  
+                autoComplete="off"  
+                required  
+                value={toPersianDigits(verificationCode.join(''))}  
+                onChange={handleInputChange}  //chnage
+                // className={error ? 'error' : ''} 
+              />  
+            <label htmlFor="phoneNumber" className={error ? 'error' : ''}>کد تایید</label>  
+            </div> 
+            </div>
+          </div>
+          <DialogTitle>
+          <h1>
+            <span className="editNumber" onClick={() => setStep(Step.PHONE)}>
+            <svg  xmlns="http://www.w3.org/2000/svg"  ><path d="M9 1.99999C9.55228 1.99999 10 2.4477 10 2.99999C10 3.55227 9.55228 3.99999 9 3.99999H3C2.55291 3.99999 2 4.5529 2 4.99999V17.1905C2 17.6376 2.36244 18 2.80952 18H15C15.4471 18 16 17.4471 16 17V11C16 10.4477 16.4477 9.99999 17 9.99999C17.5523 9.99999 18 10.4477 18 11V17C18 18.5516 16.5517 20 15 20H2.80952C1.25787 20 0 18.7421 0 17.1905V4.99999C0 3.44833 1.44834 1.99999 3 1.99999H9ZM8.53637 12.7637L16.9358 4.36431C17.2948 4.0053 17.2948 3.42324 16.9358 3.06424C16.5767 2.70523 15.9947 2.70523 15.6357 3.06424L7.2363 11.4636L6.80294 13.197L8.53637 12.7637ZM18.35 1.65002C19.49 2.79008 19.49 4.63847 18.35 5.77852L9.75473 14.3738C9.62657 14.5019 9.46599 14.5928 9.29015 14.6368L5.67111 15.5416C4.93873 15.7247 4.27533 15.0613 4.45843 14.3289L5.36319 10.7098C5.40715 10.534 5.49807 10.3734 5.62623 10.2453L14.2215 1.65002C15.3615 0.50997 17.2099 0.50997 18.35 1.65002Z"></path></svg>&nbsp;
+              اصلاح شماره
+            </span>
+          </h1>
+          </DialogTitle>
+          <form id="verification-form" style={{ width: '350px', margin: '0 auto' }}>
+          <div className="countdown-container">  
+              {!isFinished ? (  
+                <div className="flex-center">  
+                 <span className='wait-title'>&nbsp;شکیبا باشید&nbsp;</span>  
+                 <h2 >{toPersianDigits(formatTime(timeLeft))} </h2>
+                 
+                    
+                </div>  
+              ) : (  
+                <div className='resend-message'>   
+                  <h1>کد تأیید را دریافت نکردید؟</h1>   
+                  <h3  onClick={handleResendCode}>ارسال دوباره</h3>  
+                </div> 
+              )}  
+            </div>  
+            <button
+              type="submit"
+              className={`btn-primary ${isVerificationCodeEntered ? 'disabled' : ''}`}
+              disabled={isVerificationCodeEntered }
+              onClick={(e) => {
+                e.preventDefault();
+                handleCodeSubmit();
+              }}
+            >
+              عضویت            </button></form>
         </>
       );
       default:
