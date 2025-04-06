e735dc805734bc0cb3a7554f1ca5198b4f70de8b
tiredpancake <119611305+tiredpancake@users.noreply.github.com>
2025-04-06 23:33:20 +0330

    fix : fix darkmode color

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -22,7 +22,7 @@ export default function LoginModal({ open, onClose }) {
 
   const { isDarkMode, toggleDarkMode } = useTheme(); // Get both isDarkMode and toggleDarkMode from context  
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [step, setStep] = useState<Step>(Step.PHONE);
+  const [step, setStep] = useState<Step>(Step.REGISTER);
   const [timeLeft, setTimeLeft] = useState(120);
   const [isFinished, setIsFinished] = useState(false);
   const [verificationCode, setVerificationCode] = useState<string[]>(Array(4).fill(null));
@@ -84,7 +84,7 @@ export default function LoginModal({ open, onClose }) {
         '& .MuiDialog-paper': {
           borderRadius: '26px',
           overflow: 'hidden',
-          backgroundColor: isDarkMode ? '#333' : '#fff',
+          backgroundColor: isDarkMode ? '#191919' : '#fff',
           color: isDarkMode ? '#fff' : '#000',
           justifyContent: 'center',
         },
