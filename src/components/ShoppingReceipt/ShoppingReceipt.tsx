b10005f023def84af008c7de8bfca1a90ff643dc
dorsamrh <dorsa.mrh82@gmail.com>
2025-04-17 12:26:48 +0330

    add : receipt card box

--- src/components/ShoppingReceipt/ShoppingReceipt.tsx
+++ src/components/ShoppingReceipt/ShoppingReceipt.tsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from "react";
 
 const Receipt: React.FC = () => {  
     return (  
-        <div className="font-vazir text-lg">  
-        سلام! این یک متن با فونت وزیر است.  
-      </div>
+        <div className="box-content ml-10 my-10 mb-10 min-h-130 w-90 rounded-2xl bg-white shadow-[5px_7px_5px_rgba(0,0,0,0.25)]"> 
+             
+        </div>  
     );  
   };  
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
-module.exports={
-  content:[
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {  
+  content: [  
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",  
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",  
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",  
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",  
+  ],  
   theme: {  
     extend: {  
       fontFamily: {  
         vazir: ['Vazirmatn', 'sans-serif'],  
-      },  
+      },   
+      colors: {  
+        'custom-bg': '#f5f5f5',   
+      },    
     },  
-  }, 
-  plugins:[],
-}
+  },   
+  plugins: [],  
+};  
