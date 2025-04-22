61421d2856f8a62786fc54b1181981f1c5933c42
dorsamrh <dorsa.mrh82@gmail.com>
2025-04-22 20:02:36 +0330

    fix : onclick card

---
+++ src/components/ProductPage/ProductPage.tsx
@@ -0,0 +1,17 @@
+import { PhoneOutlined } from "@mui/icons-material";
+import { DialogActions, Button } from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "./login.module.scss";
+
+export default function ProductPage({ open, onClose }) {
+    
+    const [isFinished, setIsFinished] = useState(false);
+
+
+    return (
+        <>
+            <div>salam</div>
+        </>
+    );
+}
