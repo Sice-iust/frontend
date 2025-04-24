3f4f44fd2fd75ad7fa83b2b1912f50d259e0a039
dorsamrh <dorsa.mrh82@gmail.com>
2025-04-24 12:15:01 +0330

    fix : get comments from back

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,14 +1,16 @@
 import React, { useState,useEffect } from "react";  
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button ,IconButton} from "@mui/material";  
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button ,IconButton, Slider} from "@mui/material";  
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import axios from 'axios';  
 import Image from 'next/image'; 
-import { FaStar } from "react-icons/fa";  
+import { FaStar } from "react-icons/fa"; 
+import Slider from "react-slick"; 
 
 export default function ProductPage({ open, onClose, itemid }) {  
     const [isFinished, setIsFinished] = useState(false);  
     const [data, setData] = useState(null); 
     const [userdata, setuserData] = useState(null);
+    const [comments, setComments] = useState(null);
     const convertToPersianNumbers = (num: string | number): string => {  
         const persianDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];  
         return num.toString().replace(/\d/g, (digit) => persianDigits[parseInt(digit, 10)]);  
@@ -37,7 +39,7 @@ export default function ProductPage({ open, onClose, itemid }) {
     useEffect(() => {  
         const fetchDatauser = async () => {  
             try {  
-                const response = await axios.get("https://nanziback.liara.run/user/cart/quantity", {   
+                const response = await axios.get("https://nanziback.liara.run/user/cart/quantity/", {   
                     headers: { Authorization: `Bearer ${localStorage.getItem('token')}`, "Content-Type": "application/json", }
                 });  
                 console.log("item id : ",itemid);
@@ -58,6 +60,24 @@ export default function ProductPage({ open, onClose, itemid }) {
             fetchDatauser();  
         }  
     }, [itemid]);  
+
+    useEffect(() => {  
+        const fetchcomments = async () => {  
+            try {  
+                const response = await axios.get(`https://nanziback.liara.run/product/comments/${itemid}/`, {   
+                    headers: { "Content-Type": "application/json", }
+                });  
+                console.log("Response Data:", response.data);   
+            } catch (err) {  
+                console.error("Error fetching data:", err);  
+            } 
+        };  
+    
+        if (itemid) {  
+            fetchcomments();  
+        }  
+    }, [itemid]); 
+
     const handleAdd = async (itemId) => {
         const token = localStorage.getItem('token'); 
         try {
@@ -102,6 +122,38 @@ export default function ProductPage({ open, onClose, itemid }) {
           });
           alert("Cart updated!");
     };  
+
+    const settings = {
+        dots: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
+
+
     return (  
         <Dialog 
             open={open} 
@@ -133,88 +185,106 @@ export default function ProductPage({ open, onClose, itemid }) {
         <hr className="border-t border-gray-700" /> 
         {data ? (
             <>
-            <div className="flex flex-row-reverse">
-                <Image
-                    className="rounded-2xl mt-5 mb-10 mr-7 ml-4"
-                    src={data.photo_url}
-                    alt="productImg"
-                    width={300}
-                    height={200}
-                />
-                <div className="flex flex-col">
-                    <div className="flex flex-row justify-between w-full">  
-                        <div className="mt-6 box-content rounded-2xl bg-[#d9d9d9] w-auto h-7 ml-10 mt-1">  
-                            <span className="flex flex-row text-xl font-vazir ml-3 mb-1">  
-                                {convertToPersianNumbers(data.average_rate)}  
-                                <FaStar className="m-1 mr-3" color="orange" />  
-                            </span>   
-                        </div>  
-                        <span className="font-vazir font-bold text-2xl mt-6">{data.name}</span>  
-                    </div>  
-                    <div className="mt-7 ml-10 font-vazir text-lg font-medium text-right text-justify">{data.description}</div>
-                    <div className={`box-contetnt rounded-2xl bg-${data.color}-300 border-1 p-2 mt-7 ml-10 font-vazir text-lg font-semibold text-right text-justify`}>{data.box_color}</div>
-                    <div className="flex flex-row-reverse mt-9 justify-between">  
-                            <div className='flex flex-col'>  
-                                <div className="font-vazir text-lg text-right font-semibold text-xl">  
-                                    {convertToPersianNumbers(Math.round(parseFloat(data.discounted_price)).toLocaleString())} :قیمت  
-                                </div>  
-                                {data.discount > 0 && (  
-                                    <div className="flex flex-row-reverse mr-5">  
-                                        <div className="bg-[#F18825] text-lg w-9 h-5 text-white text-[14px] pl-1.5 rounded-md">  
-                                            %{convertToPersianNumbers(data.discount)}  
-                                        </div>  
-                                        <div className="mr-2 text-gray-500 line-through text-lg">  
-                                            {convertToPersianNumbers(Math.round(Number(data.price)).toLocaleString())}  
-                                        </div>  
-                                    </div>  
-                                )}  
+            <div className="fex flex-col">
+                <div className="flex flex-row-reverse">
+                    <Image
+                        className="rounded-2xl mt-5 mb-10 mr-7 ml-4"
+                        src={data.photo_url}
+                        alt="productImg"
+                        width={300}
+                        height={200}
+                    />
+                    <div className="flex flex-col">
+                        <div className="flex flex-row justify-between w-full">  
+                            <div className="mt-6 box-content rounded-2xl bg-[#d9d9d9] w-auto h-7 ml-10 mt-1">  
+                                <span className="flex flex-row text-xl font-vazir ml-3 mb-1">  
+                                    {convertToPersianNumbers(data.average_rate)}  
+                                    <FaStar className="m-1 mr-3" color="orange" />  
+                                </span>   
                             </div>  
-                            { userdata === null || userdata === 0 ? (  
-                                <button  
-                                    className={` ${data.stock==0 ? "bg-gray-300 cursor-not-allowed" : "bg-[#F18825] hover:bg-orange-400 transition duration-300 hover:scale-110"} rounded-xl w-30 h-12 text-white text-2xl font-vazir font-md mr-24 ml-10 mb-5`}  
-                                    onClick={() => handleAdd(data.id)}  
-                                    disabled={ data.stock==0}  
-                                >  
-                                    افزودن  
-                                </button>  
-                            ) : (  
-                                <div className="flex mr-19 mt-2 space-x-2">  
-                                    <button  
-                                        className={`bg-white ml-5 border-3 ${userdata >= data.stock ? "border-gray-300 text-gray-300 cursor-not-allowed" : "border-green-500 text-green-500 cursor-pointer"} font-semibold text-3xl w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-200 ${userdata >= data.stock ? "cursor-not-allowed hover:bg-white" : "hover:bg-green-500 hover:text-white hover:scale-110"}`}                                        
-                                        onClick={() => incrementQuantity(data.id)}  
-                                        disabled={userdata >= data.stock}  
-                                    >  
-                                        +  
-                                    </button>  
-                                    <span className="text-lg font-semibold">{convertToPersianNumbers(userdata || 0) || 0}</span>  
-                                    {userdata === 1 ? (  
-                                        <button  
-                                            className="bg-white cursor-pointer border-3 border-gray-300 text-gray-400 font-semibold text-3xl w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-200 hover:bg-gray-300 hover:text-gray-500 hover:scale-110"  
-                                            onClick={() => {  
-                                                removeItem(data.id);
-                                            }}  
-                                        >  
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  
-                                                <path d="M3 6h18" />  
-                                                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />  
-                                                <path d="M10 11v6" />  
-                                                <path d="M14 11v6" />  
-                                                <path d="M5 6h14l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6z" />  
-                                            </svg>  
-                                        </button>  
-                                    ) : (  
-                                        <button  
-                                            className="bg-white cursor-pointer border-3 border-red-500 text-red-500 font-semibold text-3xl w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-200 hover:bg-red-500 hover:text-white hover:scale-110"  
-                                            onClick={() => decrementQuantity(data.id)}  
-                                            disabled={userdata <= 1}  
-                                        >  
-                                            <span className="text-xl">-</span>  
-                                        </button>  
+                            <span className="font-vazir font-bold text-2xl mt-6">{data.name}</span>  
+                        </div>  
+                        <div className="mt-7 ml-10 font-vazir text-lg font-medium text-right text-justify">{data.description}</div>
+                        <div className={`box-contetnt rounded-2xl bg-${data.color}-300 border-1 p-2 mt-7 ml-10 font-vazir text-lg font-semibold text-right text-justify`}>{data.box_color}</div>
+                        <div className="flex flex-row-reverse mt-9 justify-between">  
+                                <div className='flex flex-col'>  
+                                    <div className="font-vazir text-lg text-right font-semibold text-xl">  
+                                        {convertToPersianNumbers(Math.round(parseFloat(data.discounted_price)).toLocaleString())} :قیمت  
+                                    </div>  
+                                    {data.discount > 0 && (  
+                                        <div className="flex flex-row-reverse mr-5">  
+                                            <div className="bg-[#F18825] text-lg w-9 h-5 text-white text-[14px] pl-1.5 rounded-md">  
+                                                %{convertToPersianNumbers(data.discount)}  
+                                            </div>  
+                                            <div className="mr-2 text-gray-500 line-through text-lg">  
+                                                {convertToPersianNumbers(Math.round(Number(data.price)).toLocaleString())}  
+                                            </div>  
+                                        </div>  
                                     )}  
                                 </div>  
-                            )}  
-                        </div>                    
+                                { userdata === null || userdata === 0 ? (  
+                                    <button  
+                                        className={` ${data.stock==0 ||  userdata >= data.stock? "bg-gray-300 cursor-not-allowed" : "bg-[#F18825] hover:bg-orange-400 transition duration-300 hover:scale-110"} rounded-xl w-30 h-12 text-white text-2xl font-vazir font-md mr-24 ml-10 mb-5`}  
+                                        onClick={() => handleAdd(data.id)}  
+                                        disabled={ data.stock==0}  
+                                    >  
+                                        افزودن  
+                                    </button>  
+                                ) : (  
+                                    <div className="flex mr-19 space-x-2 ml-5 mb-5">  
+                                        <button  
+                                            className={`bg-white ml-5 border-3 ${userdata >= data.stock ? "border-gray-300 text-gray-300 cursor-not-allowed" : "border-green-500 text-green-500 cursor-pointer"} font-semibold text-3xl w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-200 ${userdata >= data.stock ? "cursor-not-allowed hover:bg-white" : "hover:bg-green-500 hover:text-white hover:scale-110"}`}                                        
+                                            onClick={() => incrementQuantity(data.id)}  
+                                            disabled={userdata >= data.stock}  
+                                        >  
+                                            +  
+                                        </button>  
+                                        <span className="text-lg font-semibold">{convertToPersianNumbers(userdata || 0) || 0}</span>  
+                                        {userdata === 1 ? (  
+                                            <button  
+                                                className="bg-white cursor-pointer border-3 border-gray-300 text-gray-400 font-semibold text-3xl w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-200 hover:bg-gray-300 hover:text-gray-500 hover:scale-110"  
+                                                onClick={() => {  
+                                                    removeItem(data.id);
+                                                }}  
+                                            >  
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  
+                                                    <path d="M3 6h18" />  
+                                                    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />  
+                                                    <path d="M10 11v6" />  
+                                                    <path d="M14 11v6" />  
+                                                    <path d="M5 6h14l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6z" />  
+                                                </svg>  
+                                            </button>  
+                                        ) : (  
+                                            <button  
+                                                className="bg-white cursor-pointer border-3 border-red-500 text-red-500 font-semibold text-3xl w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-200 hover:bg-red-500 hover:text-white hover:scale-110"  
+                                                onClick={() => decrementQuantity(data.id)}  
+                                                disabled={userdata <= 1}  
+                                            >  
+                                                <span className="text-xl">-</span>  
+                                            </button>  
+                                        )}  
+                                    </div>  
+                                )}  
+                            </div>                   
+                    </div> 
+
                 </div>
+            <hr className="border-t border-gray-700 mb-10" /> 
+            <div className="flex justify-end">  
+                <span className="font-vazir font-bold text-2xl mr-10">نظرات کاربران</span>  
+            </div>  
+            {comments ?( comments.map(comment => (
+                <>
+                    <Slider {...settings}>
+
+                    </Slider>
+                </>
+            ))
+            ) : (
+                <>
+                </>
+            )}
             </div>
             </>
         ) : (
