811c56e2c565390c098357e48d316a0f28db1dca
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-05 21:37:38 +0330

    fix : content in addresspart

--- src/app/OrderSubmission/AddressPart/AddressPart.tsx
+++ src/app/OrderSubmission/AddressPart/AddressPart.tsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useTheme } from "../../theme"
 import { IoLocationOutline } from "react-icons/io5";
+import { FaRegEdit } from "react-icons/fa";
 
 export default function AddressPart() {
 
   const { isDarkMode } = useTheme();
 
   return (
-    <div className='box-content bg-white w-full ml-10 mr-10 mt-10'>
-        <div className='flex flex-row justify-between'>
-            <IoLocationOutline className='text-color-[#F18825] mr-5'/>
+    <div className='box-content bg-white rounded-2xl h-auto '>
+        <div className='flex flex-col'>
+            <div className='flex flex-row-reverse justify-between mr-5 ml-5'>
+                <div className='flex flex-row-reverse mt-3 gap-1 '>
+                    <IoLocationOutline className='h-5 w-5 text-[#F18825]'/>
+                    <span className='font-semibold text-xl'>آدرس</span>
+                </div>
+                <div className='flex flex-row-reverse mt-3 gap-2'>
+                    <FaRegEdit className="h-4 w-4 text-green-600" />
+                    <span className='font-semibold text-md text-green-600'>تغییر آدرس</span>
+                </div> 
+            </div>
+            <div className='flex flex-row-reverse'>
+                <span></span>
+            </div>
         </div>
-
     </div>
   );
 }
--- src/app/OrderSubmission/page.tsx
+++ src/app/OrderSubmission/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Receipt from '../ShoppingReceipt/ShoppingReceipt';
 import { useTheme } from "../theme"
+import AddressPart from './AddressPart/AddressPart';
 
 export default function OrderSubmission() {
 
@@ -10,10 +11,12 @@ export default function OrderSubmission() {
 
   return (
     <div className={`flex-shrink-0 ${isDarkMode ? "bg-[#383535]" : "bg-[#f5f5f5]"} flex flex-row`}>
-      <div className={`flex-shrink-0 ${isDarkMode ? "bg-[#383535]" : "bg-[#f5f5f5]"} hidden sm:block`}>
-        <Receipt />
-      </div>
-
+        <div className={`flex-shrink-0 ${isDarkMode ? "bg-[#383535]" : "bg-[#f5f5f5]"} hidden sm:block`}>
+            <Receipt />
+        </div>
+        <div className='flex flex-col w-full m-10 '>
+            <AddressPart/>
+        </div>
     </div>
   );
 }
