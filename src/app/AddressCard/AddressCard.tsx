ace4bf599fb6f24feb295e8e001bcd17f34c9b20
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-05 22:53:25 +0330

    fix : button in address card

--- src/app/AddressCard/AddressCard.tsx
+++ src/app/AddressCard/AddressCard.tsx
@@ -10,20 +10,20 @@ export default function AddressCard({ id,title, address, isSelected,isprofile,na
 
     return (
         <>
-            <div className={`box-content w-full border-1 rounded-2xl ${isSelected ? "border-green-600" : "border-black"} `}>
+            <div className={`box-content w-full border-1 rounded-2xl ${isSelected ? "border-green-600" : "border-black"} mb-5 `}>
                 <div className="flex flex-col">
                     <div className="flex flex-row-reverse justify-between mr-10 mt-2 ml-5">
                         <span className="font-semibold text-lg text-right">{title}</span>
                         <div className="flex flex-row-reverse gap-4">
-                            <FaRegEdit className=" h-4 w-4 text-green-600"/>
-                            <HiOutlineTrash className="h-4 w-4 text-red-500"/>
+                            <FaRegEdit className=" h-5 w-5 text-green-600"/>
+                            <HiOutlineTrash className="h-5 w-5 text-red-500"/>
                         </div>  
                     </div>
-                    <div className="flex flex-row-reverse gap-2">
-                    <button className={`h-5 w-5 mr-3 rounded-full border-1
+                    <div className="flex flex-row-reverse gap-2 mb-3">
+                    <button className={`h-5 w-5 mr-3 mt-1 rounded-full border-1
                                             ${isSelected ? "bg-green-600" : "bg-white"}`}></button>
-                        <span className="text-write overflow-hidden break-words 
-                                         font-medium text-md text-gray-500">{address}</span>
+                        <span className="text-right overflow-hidden break-words 
+                                         font-medium text-md text-gray-500 max-w-160 leading-[2rem]">{address}</span>
                     </div>                      
                 </div>
 
--- src/app/OrderSubmission/AddressModal/AddressModal.tsx
+++ src/app/OrderSubmission/AddressModal/AddressModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AddressCard from '../../AddressCard/AddressCard';
+import { IoMdClose } from "react-icons/io";
 
 export default function AddressModal({ onClose }) {
   const addresses = [
@@ -8,7 +9,7 @@ export default function AddressModal({ onClose }) {
       label: "خانه",
       isselect: true,
       description:
-        "تهران، شهرفرش، پایتخت، بزرگراه شهید چمران، باغ‌مشت غربی، دنباله بعد از فریزنبل، بلوک ۳۴، پلاک ۲، واحد ۲",
+        "تهران، شهرفرش، پایتخت، بزرگراه شهید چمران، باغ‌مشت غربی، دنباله بعد از فریزنبل،سلام،نمیدونم، بلوک ۳۴، پلاک ۲، واحد ۲",
     },
     {
       id: 2,
@@ -22,8 +23,13 @@ export default function AddressModal({ onClose }) {
   return (
     <div className="fixed inset-0 flex justify-center items-center z-10 ">
       <div className="bg-white p-4 rounded-lg shadow-lg  w-auto">
-        <h2 className="text-xl font-semibold mb-4">ویرایش آدرس</h2>
-
+          <div className="relative flex justify-center items-center">
+            <h2 className="text-xl font-semibold mb-4">انتخاب آدرس</h2>
+            <IoMdClose 
+              className="absolute right-0 top-0 h-7 w-5 text-gray-600 cursor-pointer" 
+              onClick={onClose} 
+            />
+          </div>
         {addresses.map((add) => (
           <AddressCard
             key={add.id}
@@ -36,13 +42,11 @@ export default function AddressModal({ onClose }) {
             phone={"ss"}
           />
         ))}
-
-        <button 
-          className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded" 
-          onClick={onClose}
-        >
-          بستن
-        </button>
+        <div className='flex flex-row-reverse'>
+          <button className='bg-[#F18825] rounded'>
+            افزودن آدرس جدید
+          </button>
+        </div>
       </div>
     </div>
   );
