5b1adb355975774cf9a2dbedbcfa853978c58c61
Masumesk <msumsanatkar@gmail.com>
2025-05-05 17:26:04 +0330

    Add : CheckoutPage

---
+++ src/app/CheckoutPage/Cart/Cart.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Cart: React.FC = () => {
+  return (
+
+     <div className="bg-[#f5f5f5]"> 
+            <div className={`box-content ml-10 mt-10 mb-10 min-h-140 w-100 rounded-2xl bg-white `}>  
+                <h2 className={`text-[25px] text-center text-black pt-5 pb-2 font-vazir font-bold`}>  
+                    سبد خرید  
+                </h2>  
+            </div>
+    </div>
+  );
+};
+
+export default Cart;
