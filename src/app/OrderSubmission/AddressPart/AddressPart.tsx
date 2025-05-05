e331c254d8bd678d08b4c24fcb07382696bff1bc
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-05 21:41:58 +0330

    fix : complete address part layout

--- src/app/OrderSubmission/AddressPart/AddressPart.tsx
+++ src/app/OrderSubmission/AddressPart/AddressPart.tsx
@@ -10,7 +10,7 @@ export default function AddressPart() {
   return (
     <div className='box-content bg-white rounded-2xl h-auto '>
         <div className='flex flex-col'>
-            <div className='flex flex-row-reverse justify-between mr-5 ml-5'>
+            <div className='flex flex-row-reverse justify-between mr-10 ml-10'>
                 <div className='flex flex-row-reverse mt-3 gap-1 '>
                     <IoLocationOutline className='h-5 w-5 text-[#F18825]'/>
                     <span className='font-semibold text-xl'>آدرس</span>
@@ -20,8 +20,8 @@ export default function AddressPart() {
                     <span className='font-semibold text-md text-green-600'>تغییر آدرس</span>
                 </div> 
             </div>
-            <div className='flex flex-row-reverse'>
-                <span></span>
+            <div className='flex flex-row-reverse mt-3 mr-15 mb-5'>
+                <span className='font-medium text-md text-gray-500'>تهران، شهرآرا، خیابان فلان، کوچه بهمان، پلاک ۱۰ واحد ۳</span>
             </div>
         </div>
     </div>
