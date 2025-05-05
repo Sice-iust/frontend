be1bc8a577e1d10d300d69a7194b7df2320659a3
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-05 22:37:40 +0330

    fix : address card margins

--- src/app/AddressCard/AddressCard.tsx
+++ src/app/AddressCard/AddressCard.tsx
@@ -8,40 +8,23 @@ import { HiOutlineTrash } from "react-icons/hi";
 
 export default function AddressCard({ id,title, address, isSelected,isprofile,name,phone }) {
 
-    const addresses = [
-      {
-        id: 1,
-        label: "خانه",
-        isselect:true,
-        description:
-          "تهران، شهرفرش، پایتخت، بزرگراه شهید چمران، باغ‌مشت غربی، دنباله بعد از فریزنبل، بلوک ۳۴، پلاک ۲، واحد ۲",
-      },
-      {
-        id: 2,
-        label: "دانشگاه",
-        isselect:false,
-        description:
-          "تهران، صنعت، غدیر، بلور، بلوار تکاوران، دانشگاه علم و صنعت ایران، دبیرستان و دانشگاه",
-      },
-    ];
-
     return (
         <>
-            <div className={`box-content w-full  ml-10 mr-10 mt-5 border-1 ${isSelected ? "border-green-600" : "border-black"} `}>
+            <div className={`box-content w-full border-1 rounded-2xl ${isSelected ? "border-green-600" : "border-black"} `}>
                 <div className="flex flex-col">
-                    <span className="font-semibold text-lg text-right mr-5">{name}</span>
-                    <div className="flex flex-row justify-between">
-                        <div className="flex-flex-row-reverse">
-                            <button className={`h-5 w-5 mr-5 rounded-full border-1
-                                                ${isSelected ? "bg-green-600" : "bg-white"}`}></button>
-                            <span className="mr-5 text-write overflow-hidden break-words 
-                                             font-medium text-md text-gray-500">{address}</span>
-                        </div>
-                        <div className="flex-flex-row-reverse">
-                            <FaRegEdit className="color-green-600"/>
-                            <HiOutlineTrash className="color-red-500"/>
-                        </div>                        
+                    <div className="flex flex-row-reverse justify-between mr-10 mt-2 ml-5">
+                        <span className="font-semibold text-lg text-right">{title}</span>
+                        <div className="flex flex-row-reverse gap-4">
+                            <FaRegEdit className=" h-4 w-4 text-green-600"/>
+                            <HiOutlineTrash className="h-4 w-4 text-red-500"/>
+                        </div>  
                     </div>
+                    <div className="flex flex-row-reverse gap-2">
+                    <button className={`h-5 w-5 mr-3 rounded-full border-1
+                                            ${isSelected ? "bg-green-600" : "bg-white"}`}></button>
+                        <span className="text-write overflow-hidden break-words 
+                                         font-medium text-md text-gray-500">{address}</span>
+                    </div>                      
                 </div>
 
             </div>
--- src/app/OrderSubmission/AddressModal/AddressModal.tsx
+++ src/app/OrderSubmission/AddressModal/AddressModal.tsx
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
-import { useTheme } from "../../theme"
-import { IoLocationOutline } from "react-icons/io5";
-import { FaRegEdit } from "react-icons/fa";
+import React from 'react';
+import AddressCard from '../../AddressCard/AddressCard';
 
-export default function AddressModal  ({ onClose }){
-  
-    return(
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-4 rounded-lg shadow-lg max-w-sm w-full">
+export default function AddressModal({ onClose }) {
+  const addresses = [
+    {
+      id: 1,
+      label: "خانه",
+      isselect: true,
+      description:
+        "تهران، شهرفرش، پایتخت، بزرگراه شهید چمران، باغ‌مشت غربی، دنباله بعد از فریزنبل، بلوک ۳۴، پلاک ۲، واحد ۲",
+    },
+    {
+      id: 2,
+      label: "دانشگاه",
+      isselect: false,
+      description:
+        "تهران، صنعت، غدیر، بلور، بلوار تکاوران، دانشگاه علم و صنعت ایران، دبیرستان و دانشگاه",
+    },
+  ];
+
+  return (
+    <div className="fixed inset-0 flex justify-center items-center z-10 ">
+      <div className="bg-white p-4 rounded-lg shadow-lg  w-auto">
         <h2 className="text-xl font-semibold mb-4">ویرایش آدرس</h2>
-        {/* Your form or content goes here */}
+
+        {addresses.map((add) => (
+          <AddressCard
+            key={add.id}
+            id={add.id}
+            title={add.label}
+            address={add.description}
+            isSelected={add.isselect}
+            isprofile={false}
+            name={"ss"}
+            phone={"ss"}
+          />
+        ))}
+
         <button 
           className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded" 
           onClick={onClose}
@@ -18,5 +45,5 @@ export default function AddressModal  ({ onClose }){
         </button>
       </div>
     </div>
-    )
-};
+  );
+}
