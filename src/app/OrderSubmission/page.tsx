aa145a0b1f3468cfb78a499319c63d51d176567a
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-05 20:11:49 +0330

    add : order submission page

---
+++ src/app/OrderSubmission/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Receipt from '../ShoppingReceipt/ShoppingReceipt';
+import { useTheme } from "../theme"
+
+export default function Bread() {
+
+  const { isDarkMode } = useTheme();
+
+  return (
+    <div className={`flex-shrink-0 ${isDarkMode ? "bg-[#383535]" : "bg-[#f5f5f5]"} flex flex-row`}>
+      <div className={`flex-shrink-0 ${isDarkMode ? "bg-[#383535]" : "bg-[#f5f5f5]"} hidden sm:block`}>
+        <Receipt />
+      </div>
+
+    </div>
+  );
+}
