955d2bb19bdefed5c2a12895d61f9504cb477951
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-06 23:43:09 +0330

    add : time card

--- src/app/OrderSubmission/SendingPart/TimeChoosing.tsx
+++ src/app/OrderSubmission/SendingPart/TimeChoosing.tsx
@@ -1,6 +1,68 @@
 import React from 'react';
 
 const TimeChoosing: React.FC = () => {
+    const times = [
+        {
+          id: 1,
+          label: "شنبه",
+          date:"۱۰ اسفند",
+          isSelected:false,
+        },
+        {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+          {
+            id: 1,
+            label: "شنبه",
+            date:"۱۰ اسفند",
+            isSelected:false,
+          },
+      ];
   return (
 
       <p>اینجا محتوای مربوط به تحویل فوری قرار می‌گیرد.</p>
---
+++ src/app/OrderSubmission/TimeCard/TimeCard.tsx
@@ -0,0 +1,18 @@
+import { PhoneOutlined } from "@mui/icons-material";
+import { DialogActions, Button } from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
+import { FaRegEdit } from "react-icons/fa";
+import { HiOutlineTrash } from "react-icons/hi";
+import { FaCheck } from "react-icons/fa"; 
+
+
+export default function TimeCard({ id,title, date, isSelected }) {
+
+    return (
+        <>
+            
+        </>
+    );
+}
+
