362e875731d06c9019a2838c3415e3b78b322e9f
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-07 00:03:46 +0330

    fix : time card

--- src/app/OrderSubmission/SendingPart/TimeChoosing.tsx
+++ src/app/OrderSubmission/SendingPart/TimeChoosing.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { IoMdTime } from "react-icons/io";
+import TimeCard from '../TimeCard/TimeCard';
 
 const TimeChoosing: React.FC = () => {
     const times = [
         {
           id: 1,
-          label: "شنبه",
+          label: "چهارشنبه",
           date:"۱۰ اسفند",
-          isSelected:false,
+          isSelected:true,
         },
         {
             id: 1,
@@ -64,8 +66,24 @@ const TimeChoosing: React.FC = () => {
           },
       ];
   return (
+    <>
+        <div className='flex flex-col mr-10 mt-3'>
+            <div className='flex flex-row-reverse mt-3 gap-1 '>
+              <IoMdTime className='h-5 w-5 text-[#F18825]'/>
+              <span className='font-semibold text-xl'>انتخاب زمان تحویل</span>
+            </div>
+            <div className='flex flex-row-reverse mt-6 mr-10 ml-10 mb-6 gap-7 overflow-x-auto '>
+                {times.map((time)=>
+                    <TimeCard 
+                        id={time.id} 
+                        title={time.label} 
+                        date={time.date} 
+                        isSelected={time.isSelected}/>
+                )}               
+            </div>           
 
-      <p>اینجا محتوای مربوط به تحویل فوری قرار می‌گیرد.</p>
+        </div>
+    </>
   );
 };
 
--- src/app/OrderSubmission/TimeCard/TimeCard.tsx
+++ src/app/OrderSubmission/TimeCard/TimeCard.tsx
@@ -11,7 +11,14 @@ export default function TimeCard({ id,title, date, isSelected }) {
 
     return (
         <>
-            
+            <div className={`box-content border-1 w-20 h-27 mb-5 rounded-2xl flex-shrink-0
+                            ${isSelected? "border-black bg-orange-300" : "border-gray-500 bg-white"}`}>
+                <div className="flex flex-col mt-2 gap-1 justify-center items-center">
+                    <span className="font-semibold text-lg">{title}</span>
+                    <span className="font-md text-md text-gray-500">{date}</span>
+                    <div className="box-content bg-gray-300 rounded-4xl text-sm p-1">ارسال رایگان</div>
+                </div>
+            </div>  
         </>
     );
 }
