d156b35168c871bf4052b22f0a509df7b4228058
dorsamrh <dorsa.mrh82@gmail.com>
2025-05-06 19:29:56 +0330

    fix : name of product in card

--- src/app/OrderSubmission/SummaryPart/SummaryPart.tsx
+++ src/app/OrderSubmission/SummaryPart/SummaryPart.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { useTheme } from "../../theme"
-import { IoLocationOutline } from "react-icons/io5";
-import { FaRegEdit } from "react-icons/fa";
 import AddressModal from "../AddressModal/AddressModal"
 import { CgNotes } from "react-icons/cg";
+import { useCart } from "../../../context/Receiptcontext";
+import Image from 'next/image';
 
 export default function SummaryPart() {
   const { isDarkMode } = useTheme();
-  
+  const { cartItems, counts, totalDiscount, totalActualPrice, loading ,
+      incrementQuantity, decrementQuantity, removeItem} = useCart(); 
   const [isModalOpen, setModalOpen] = useState(false);
   
   const handleEditClick = () => {
@@ -27,16 +28,19 @@ export default function SummaryPart() {
               <CgNotes className='h-5 w-5 text-[#F18825]'/>
               <span className='font-semibold text-xl'>خلاصه سفارش</span>
             </div>
-            <div onClick={handleEditClick} className='flex flex-row-reverse mt-3 gap-2 cursor-pointer'>
-              <FaRegEdit className="h-4 w-4 text-green-600" />
-              <span className='font-semibold text-md text-green-600'>تغییر آدرس</span>
-            </div> 
           </div>
-          <div className='flex flex-row-reverse mt-3 mr-15 mb-5'>
-            <span className='font-medium text-md text-gray-500'>
-              تهران، شهرآرا، خیابان فلان، کوچه بهمان، پلاک ۱۰ واحد ۳
-            </span>
-          </div>
+          <div className='flex flex-row-reverse mt-6 mr-10 ml-10 mb-6 gap-7 overflow-x-auto max-w-230'>
+                {cartItems.map((item)=>(
+                    <div className='box-content border-1 min-w-50 h-55 w-auto rounded-2xl flex-shrink-0 '>
+                        <div className='flex flex-col mt-3 gap-3 justify-center items-center'>
+                            {/* <Image src={item.product.photo} alt='bread'/> */}
+                            <span className='p-3 text-right text-md font-semibold'>{item.product.name}</span>
+                        </div>
+                    </div>
+                    
+                ))
+                }
+            </div>
         </div>
       </div>
 
